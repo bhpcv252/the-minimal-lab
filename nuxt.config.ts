470015fcd22838096ed5ea13878dc0fabd8c0fd9
Sonu Singh <bhpcv252@gmail.com>
2022-03-17 15:54:20 +0530

    global css fixed

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,12 +2,7 @@ import { defineNuxtConfig } from 'nuxt3'
 
 // https://v3.nuxtjs.org/docs/directory-structure/nuxt.config
 export default defineNuxtConfig({
-  meta: {
-    link: [
-      {
-        rel: 'stylesheet',
-        href: 'assets/styles/global.scss'
-      }
-    ]
-  }
+  css: [
+    '@/assets/styles/global.scss'
+  ]
 })
